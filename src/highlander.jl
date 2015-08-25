564a02e6b677a54faa6861765553e5015dd2b78a
dfdx <faithlessfriend@gmail.com>
2015-08-26 00:09:34 +0300

    well-established experiment - 127 seconds per 100 records

--- src/highlander.jl
+++ src/highlander.jl
@@ -3,40 +3,42 @@ using Requests
 using URIParser
 using Gadfly
 
-geturl(line) = strip(split(line, ",")[4], '\"')
+## geturl(line) = strip(split(line, ",")[4], '\"')
 
-function getallurls(filename)
-    lines = split(readall(filename), "\n")[2:end-1]
-    urls = Array(String, length(lines))
-    for i=1:length(lines)
-        urls[i] = "http://" * geturl(lines[i])
-    end
-    return urls
-end
+## function getallurls(filename)
+##     lines = split(readall(filename), "\n")[2:end-1]
+##     urls = Array(String, length(lines))
+##     for i=1:length(lines)
+##         urls[i] = "http://" * geturl(lines[i])
+##     end
+##     return urls
+## end
 
 
-function resolvehost(url)
-    h = URI(url).host
-    ip = string(Base.getaddrinfo(h))
-    replace(url, h, ip)
-end
+## function resolvehost(url)
+##     h = URI(url).host
+##     ip = string(Base.getaddrinfo(h))
+##     replace(url, h, ip)
+## end
 
-function resolvehosts(urls)
-    result = String[]
-    @sync for url in urls
-        @async try
-            resolved = resolvehost(strip(url))
-            push!(result, resolved)
-        catch
-            println("Can't resolve $url")
-        end
-    end
-    return result
-end
+## function resolvehosts(urls)
+##     result = String[]
+##     @sync for url in urls
+##         @async try
+##             resolved = resolvehost(strip(url))
+##             push!(result, resolved)
+##         catch
+##             println("Can't resolve $url")
+##         end
+##     end
+##     return result
+## end
 
-function get_stream(url::String)
-    Requests.open_stream(URI(url), Dict(), "", "GET")
-end
+## function get_stream(url::String)
+##     Requests.open_stream(URI(url), Dict(), "", "GET")
+## end
+
+# @time urls = resolvehosts(open(readlines, expanduser("../domains.txt"))[1:5000])
 
 
 function fetch_urls(urls; timeout=Inf)
@@ -48,9 +50,9 @@ function fetch_urls(urls; timeout=Inf)
                 t = @elapsed resp = get(urls[i]; timeout=timeout)
                 statuses[i] = resp.status
                 times[i] = t
-                println("OK")
+                print("OK\n")
             catch
-                println("Error")
+                print("Error\n")
             end
         end
     end
@@ -58,13 +60,10 @@ function fetch_urls(urls; timeout=Inf)
 end
 
 
-function main()
-
-    # @time urls = resolvehosts(open(readlines, expanduser("../domains.txt"))[1:5000])
-    @time urls = [strip(l) for l in open(readlines, expanduser("../ips.txt"))]
-
-    @time (statuses, times) = fetch_urls(urls[1:100])
-
+function main()    
+    @time urls = [strip(l) for l in
+                  open(readlines, expanduser("~/Dropbox/ips.txt"))][1:100]
+    @time (statuses, times) = fetch_urls(urls, timeout=2)
     plot(x=[i for i=1:length(times)], y=times)
-    
 end
+
